2c9372c333c2a0ed7d1c224fadd74c10e20544bf
Benjamin Klüglein <scheibenkaes@googlemail.com>
2012-02-11 15:15:29 +0100

    removed clutter

--- init.el
+++ init.el
@@ -8,17 +8,6 @@
 (set-keyboard-coding-system 'iso-latin-1)
 (if (window-system)
     (progn
-      ;; "fix" the broken keyboard                                        
-;      (global-set-key "\M-l" '(lambda () (interactive) (insert "@")))
-;      (global-set-key "\M-5" '(lambda () (interactive) (insert "[")))
-;      (global-set-key "\M-6" '(lambda () (interactive) (insert "]")))
-;      (global-set-key "\M-7" '(lambda () (interactive) (insert "|")))
-;      (global-set-key "\M-/" '(lambda () (interactive) (insert "\\")))
-;      (global-set-key "\M-8" '(lambda () (interactive) (insert "{")))
-;      (global-set-key "\M-9" '(lambda () (interactive) (insert "}")))
-     ; (global-set-key "\M-n" '(lambda () (interactive) (insert "~")))
       (setq mac-command-modifier 'meta)
-      (setq mac-option-modifier nil)
-      ))
+      (setq mac-option-modifier nil)))
 
-(global-linum-mode 1)
