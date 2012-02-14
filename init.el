1f00c644f036f17f49a0882c4d110cd1cd40981d
Benjamin Klüglein <scheibenkaes@googlemail.com>
2012-02-14 21:35:08 +0100

    nicer structure less clutter some enhancements in init.el

--- init.el
+++ init.el
@@ -1,14 +1,15 @@
 (setq x-select-enable-clipboard t)
- 
+(setq ispell-program-name "aspell")
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier nil)
+
+;; packages
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
 (set-keyboard-coding-system 'iso-latin-1)
-(if (window-system)
-    (progn
-      (setq mac-command-modifier 'meta)
-      (setq mac-option-modifier nil)))
 
-(setq ispell-program-name "aspell")
+;; modes
+(undo-tree-mode 1)
