69c373618bde4bfe786755e33a4a7fe9ec8178bc
Benjamin Klueglein <benjamin.klueglein@carefusion.com>
2014-10-29 15:54:42 +0100

    add server-start and fun to yank last url

--- work.el
+++ work.el
@@ -1,6 +1,12 @@
 
+(server-start)
+
 (setq explicit-shell-file-name "C:/Program Files (x86)/Git/bin/bash.exe")
 
 (load-theme 'warm-night)
 (set-cursor-color "#e67e22")
+
+(fset 'b6n-yank-last-url
+   [?\C-r ?h ?t ?t ?p ?\C-m ?\C-  ?\C-e ?\M-w ?\C-u ?\C-  ?\C-u ?\C- ])
+
 
