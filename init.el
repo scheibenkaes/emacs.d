12b4716fc9993e819ebbbb25a61fc9ac6e0b39c9
Benjamin Klüglein <scheibenkaes@googlemail.com>
2016-02-12 08:26:42 +0100

    add synosaurus custom variables

--- init.el
+++ init.el
@@ -63,6 +63,7 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
 (package-initialize)
 
 (when (not package-archive-contents)
@@ -183,3 +184,16 @@
 (global-set-key (kbd "M-g g") 'b6n/linums-while-goto-line)
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(synosaurus-backend (quote synosaurus-backend-openthesaurus))
+ '(synosaurus-choose-method (quote popup)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
