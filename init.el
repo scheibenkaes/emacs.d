140f8bc19e1a4b4497144fb13ed863480f47fb57
Benjamin Klüglein <scheibenkaes@googlemail.com>
2017-03-06 23:48:06 +0100

    add markdown mode

--- init.el
+++ init.el
@@ -63,6 +63,14 @@
   (setq blink-cursor-blinks 2)
   (load-theme 'material t))
 
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
 (use-package paredit
   :ensure t
   :config
