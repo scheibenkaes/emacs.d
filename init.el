6bd579a0e169817e29328d6df13f46941e808d23
Benjamin Klüglein <scheibenkaes@googlemail.com>
2017-03-06 22:30:17 +0100

    add projectile and dashboar

--- init.el
+++ init.el
@@ -66,6 +66,15 @@
   (add-hook 'lisp-interaction-mode-hook 'paredit-mode)
   (add-hook 'clojure-mode-hook          'paredit-mode))
 
+(use-package projectile
+  :ensure t)
+
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook)
+  (setq dashboard-startup-banner 'logo))
+
 (use-package rainbow-delimiters
   :ensure t
   :init (rainbow-delimiters-mode t))
