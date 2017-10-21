eed4c4e9e475d5e827939463ccb669c8e704648b
Benjamin Klüglein <scheibenkaes@googlemail.com>
2017-10-21 16:34:25 +0200

    typescript setup

--- init.el
+++ init.el
@@ -188,6 +188,32 @@
 (use-package web-mode
   :ensure t)
 
+(use-package typescript-mode
+  :ensure t)
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+(use-package tide
+  :ensure t
+  :config
+  ;; aligns annotation to the right hand side
+  (setq company-tooltip-align-annotations t)
+
+  ;; formats the buffer before saving
+  (add-hook 'before-save-hook 'tide-format-before-save)
+
+  (add-hook 'typescript-mode-hook #'setup-tide-mode))
+
 (use-package yasnippet
   :ensure t
   :config
