95bcc619a7bd61f0a8e1094cc7f4ebcf6a1fbd0a
Benjamin Klüglein <benjamin.klueglein@methodpark.de>
2019-08-07 15:21:07 +0200

    add C-c p as projectile binding

--- init.el
+++ init.el
@@ -152,7 +152,8 @@
 (use-package projectile
   :ensure t
   :config
-  (projectile-mode))
+  (projectile-mode)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
 
 (defun goto-dashboard ()
   "Only display the dashboard"
